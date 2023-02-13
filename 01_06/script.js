9363c0e6d2836747beeb4326e05b0e7ea25a11ad
fmau <florence@maurice-web.de>
2023-02-13 10:16:49 +0100

    add files for chapter 1

---
+++ 01_06/script.js
@@ -0,0 +1,6 @@
+const styleSheets = document.styleSheets;
+const listButton = document.getElementById("list");
+const gridButton = document.getElementById("grid");
+
+listButton.classList.add("current");
+
