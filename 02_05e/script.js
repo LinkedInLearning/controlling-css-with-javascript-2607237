958c42ad8c86e132dda56953fb5c7257c70deab8
fmau <florence@maurice-web.de>
2023-02-14 10:27:55 +0000

    changed

--- 02_05e/script.js
+++ 02_05e/script.js
@@ -1,7 +1,8 @@
 const listItem = document.querySelector(".animals li:nth-child(5) a");
 
 listItem.style.backgroundColor = "orange";
-// listItem.setAttribute("style", "font-style: italic;");
-listItem.style.fontStyle = "italic"
+//listItem.setAttribute("style", "font-style: italic");
+
+listItem.style.fontStyle = "italic";
 
 console.log(listItem.getAttribute("style"));
