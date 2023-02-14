642efa353df4087ebd2d79c5afd1178be6020489
fmau <florence@maurice-web.de>
2023-02-14 11:42:30 +0000

    changed

--- 03_05e/script.js
+++ 03_05e/script.js
@@ -1,14 +1,17 @@
 const styleSheet = document.styleSheets[0];
 
+console.log(styleSheet.cssRules[2]);
+
 let headingOpacity = styleSheet.cssRules[2].style.opacity;
+console.log(typeof(headingOpacity));
+headingOpacity = headingOpacity + 0.2;
 
 console.log(headingOpacity);
-headingOpacity = headingOpacity + 0.2;
-console.log(headingOpacity)
 
-// console.log(styleSheet.cssRules[2]);
 let typedHeadingOpacity = styleSheet.cssRules[2].styleMap.get("opacity").value;
-console.log(typedHeadingOpacity)
-console.log(typeof(typedHeadingOpacity))
+console.log(typedHeadingOpacity);
+
+console.log(typeof(typedHeadingOpacity));
+
 typedHeadingOpacity = typedHeadingOpacity + 0.2;
-console.log(typedHeadingOpacity)
+console.log(typedHeadingOpacity);
