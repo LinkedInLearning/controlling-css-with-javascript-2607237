653b7a2a7476d9dfa9de33d391002f71f82647f7
fmau <florence@maurice-web.de>
2023-02-14 11:14:30 +0000

    update

--- 03_01e/script.js
+++ 03_01e/script.js
@@ -1,2 +1,2 @@
-const styleSheet = document.styleSheets[0];
+const styleSheet =document.styleSheets[0];
 console.log(styleSheet.cssRules[2].cssText);
--- 03_04e/script.js
+++ 03_04e/script.js
@@ -1,13 +1,15 @@
 const styleSheet = document.styleSheets[0];
 
-// Ruleset text before:
-console.log(styleSheet.cssRules[2].cssText);
 
 styleSheet.cssRules[2].style.setProperty("background-color", "skyblue");
 console.log(styleSheet.cssRules[2].style.getPropertyValue("background-color"));
 styleSheet.cssRules[2].style.setProperty("max-inline-size", "65rem");
 console.log(styleSheet.cssRules[2].style.getPropertyValue("max-inline-size"));
+
+// Regeln vorher:
+console.log(styleSheet.cssRules[2].cssText);
+
 styleSheet.cssRules[2].style.removeProperty("margin-inline");
 
-// Ruleset text after:
+// Regeln danach:
 console.log(styleSheet.cssRules[2].cssText);
