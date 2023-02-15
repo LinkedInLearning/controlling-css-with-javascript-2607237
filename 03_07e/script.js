07467fcf952675184079db3a12d491033298ef6e
fmau <florence@maurice-web.de>
2023-02-15 10:00:22 +0000

    changed

--- 03_07e/script.js
+++ 03_07e/script.js
@@ -3,7 +3,8 @@ const listItem = document.querySelector(".animals li:nth-child(5) a");
 listItem.style.backgroundColor = "orange";
 console.log(listItem.style.backgroundColor);
 
-listItem.attributeStyleMap.set("font-size", "4rem");
+listItem.attributeStyleMap.set("font-size", "3rem");
 console.log(listItem.attributeStyleMap.get("font-size").value);
 console.log(listItem.attributeStyleMap.get("font-size").unit);
-console.log(listItem.attributeStyleMap.get("background-color"));
+
+console.log(listItem.attributeStyleMap.get("background-color").toString());
